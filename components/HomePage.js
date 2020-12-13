a4358d8d3bc3488c04b3ba1c2e86c79da8acd1ed
Stella Vilar <vilar.stella@gmail.com>
2020-12-13 18:39:09 +0100

    Home page installed and json file for data imported

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, ScrollView, Text } from 'react-native';
+import data from '../openapi.json';
 
 const Homepage = () => {
-
+    console.log(data[0].name)
     return (
         <View style={styles.container} >
             <Image source={require('../img/countries.png')}/>
+                <View style={styles.card}>
+                    <Text>Afrique du sud</Text>
+                </View>
         </View>
     );
 
@@ -18,7 +22,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#B6D8F9',
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    card: {
+        height: 400,
+        width: 400,
+        borderRadius: 10,
+    },
+    // flag: {
+    //     height:190,
+    //     width:290,
+    // }
 });
 
 export default Homepage;
