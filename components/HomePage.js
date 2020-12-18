e8cedfe690169d794754a8ccb9392f6ef208eb61
Stella Vilar <vilar.stella@gmail.com>
2020-12-18 23:22:09 +0100

    Horizontal scrollview installed to see all flags on homepage

--- components/HomePage.js
+++ components/HomePage.js
@@ -3,35 +3,71 @@ import { StyleSheet, Image, View, ScrollView, Text } from 'react-native';
 import data from '../openapi.json';
 
 const Homepage = () => {
-    console.log(data[0].name)
+   
     return (
         <View style={styles.container} >
             <Image source={require('../img/countries.png')}/>
-                <View style={styles.card}>
-                    <Text>Afrique du sud</Text>
-                </View>
+            <View style={{ width: 500 }}>
+                <ScrollView 
+                    pagingEnabled 
+                    horizontal 
+                    showsHorizontalScrollIndicator={false}
+                    style={{ height: 400, width: 500 }} >
+                    {
+                    data.map((flags, index) => (
+                        <View style={styles.card}>
+                            <Image 
+                                key={index}
+                                source= {{ uri: flags.flag}}
+                                style={styles.flag}
+                            />
+                            <View style={styles.infos}>
+                                <Text key={flags.name} style={styles.text}>{flags.name}</Text>
+                            </View>
+                        </View>
+                    ))
+                    }
+                </ScrollView>
+            </View>
         </View>
+    
     );
 
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
 
+    flag: {
+        width: 440,
+        height: 300,
+        resizeMode: 'cover',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        marginRight: 10
+    },
     container: {
         flex: 1,
         backgroundColor: '#B6D8F9',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    card: {
-        height: 400,
-        width: 400,
-        borderRadius: 10,
+    text: {
+        fontSize: 18,
+        color:'black',
     },
-    // flag: {
-    //     height:190,
-    //     width:290,
-    // }
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    infos: {
+        backgroundColor: 'white',
+        width: 440,
+        height: 50,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+
+    }
 });
+
 
 export default Homepage;
