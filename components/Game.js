d172ea5ccf87bee0b5e1784f48af7e5771e408ce
Stella Vilar <vilar.stella@gmail.com>
2021-01-18 23:11:55 +0100

    Shuffle array element to show several proposals for the game

--- components/Game.js
+++ components/Game.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
+import { StyleSheet, View, Text, Image } from 'react-native';
+import data from '../openapi.json';
 
 const Game = () => {
+
+    //Get random flag from the array
+    const getRandomFlag = (data) => {
+        return data[Math.floor(Math.random()*data.length)]
+    };
+    //Pick up the first element of the array:
+    const flag = getRandomFlag(data);
+    
+    //Shuffle array elements
+    const shuffle = (array) => {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+            
+        }
+        return shuffle;
+    };
+
+    shuffle(data);
+
     return (
         <View style={styles.container}>
-            <Text></Text>
+            <Text>Find the correct flag!</Text>
+            <View>
+                <Image 
+                    source= {{ uri: flag.flag}} 
+                    style={styles.flag}  
+                />
+                 {data.map((flags, index) =>
+                    <View>
+                        <Text>{flags.name}</Text>
+                    </View>
+                    ).slice(0,4)
+                }
+                <Text>{flag.name}</Text>
+            </View>
         </View>
-    )
+    );
+
 };
 
 const styles = StyleSheet.create ({
@@ -15,6 +59,11 @@ const styles = StyleSheet.create ({
         backgroundColor: '#B6D8F9',
         height: '100%',
     },
+    flag: {
+        height: 190,
+        width: '90%',
+        borderRadius: 20,
+    }
 });
 
 export default Game;
